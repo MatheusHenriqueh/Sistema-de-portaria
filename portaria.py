0dbc21b27a03a02d50b4d07113e3ccccbddea5d7
Matheus Ruivo <matheushenriquerodriguesruivo4@gmail.com>
2025-10-21 00:37:57 -0300

    Add files via upload

---
+++ portaria.py
@@ -0,0 +1,23 @@
+#Sistema de Portaria.
+#Autor: Matheus Ruivo
+#Data: 21/10/25
+
+entrada = input("Digite entrar, se quiser sair digite sair:")
+
+if entrada == 'Entrar' or entrada =='entrar' or entrada == 'ENTRAR':#Sempre colocar o valor das variável.
+    print("Bem-vindo(a)!")
+    nome = input("Digite seu nome:")
+    ap = (input("Digite seu número do apartamento:"))#Sem int!
+    senha = (input("Digite sua senha:"))
+    n = 'Matheus'
+    a =  123#Números sem aspas!!
+    s = 1234
+    print("Carregando...")
+    if (nome == n and senha == s) or ap == a:
+        print(f"Portão aberto, olá {nome}!")   
+    else:
+        print("Algo está errado!!")
+elif entrada == 'Sair' or 'sair' or 'SAIR':
+    print("Ops, você saiu do sistema!")
+else:
+    print("Digite entrar ou sair!")
